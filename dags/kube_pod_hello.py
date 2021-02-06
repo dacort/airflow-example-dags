e0bac184bff2765f0e9edfef5bc9df91ba7476b4
Damon P. Cortesi <d.lifehacker@gmail.com>
2021-02-05 22:07:18 -0800

    formattttttting

--- dags/kube_pod_hello.py
+++ dags/kube_pod_hello.py
@@ -4,47 +4,52 @@ from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOpera
 from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime.utcnow(),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime.utcnow(),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
 }
 
 dag = DAG(
-    'kubernetes_hello_world', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    "kubernetes_hello_world",
+    default_args=default_args,
+    schedule_interval=timedelta(minutes=10),
+)
 
 
-start = DummyOperator(task_id='start', dag=dag)
+start = DummyOperator(task_id="start", dag=dag)
 
-passing = KubernetesPodOperator(namespace='default',
-                          image="python:3.6",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="passing-test",
-                          task_id="passing-task",
-                          is_delete_operator_pod=True,
-                          get_logs=True,
-                          dag=dag
-                          )
+passing = KubernetesPodOperator(
+    namespace="default",
+    image="python:3.6",
+    cmds=["python", "-c"],
+    arguments=["print('hello world')"],
+    labels={"foo": "bar"},
+    name="passing-test",
+    task_id="passing-task",
+    is_delete_operator_pod=True,
+    get_logs=True,
+    dag=dag,
+)
 
-failing = KubernetesPodOperator(namespace='default',
-                          image="ubuntu:16.04",
-                          cmds=["python","-c"],
-                          arguments=["print('hello world')"],
-                          labels={"foo": "bar"},
-                          name="fail",
-                          task_id="failing-task",
-                          is_delete_operator_pod=True,
-                          get_logs=True,
-                          dag=dag
-                          )
+failing = KubernetesPodOperator(
+    namespace="default",
+    image="ubuntu:16.04",
+    cmds=["python", "-c"],
+    arguments=["print('hello world')"],
+    labels={"foo": "bar"},
+    name="fail",
+    task_id="failing-task",
+    is_delete_operator_pod=True,
+    get_logs=True,
+    dag=dag,
+)
 
-end = DummyOperator(task_id='end', dag=dag)
+end = DummyOperator(task_id="end", dag=dag)
 
 
 passing.set_upstream(start)
